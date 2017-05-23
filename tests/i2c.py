c6f8554b799317afe017e1d121203047772f9362
Chris Borrill <chris.borrill@gmail.com>
2017-05-23 21:04:59 +1200

    Some working tests

--- tests/i2c.py
+++ tests/i2c.py
@@ -2,13 +2,22 @@ class I2C():
 
     def __init__(self, interfaceNo):
         self.interfaceNo = interfaceNo
+        self._read_result = {}
+        self._read_arguments = {}
+        self._write_arguments = {}
+
+    def set_readfrom_mem_result(self, register, result_bytes):
+        self._read_result[register] = result_bytes
 
     def readfrom_mem(self, address, register, bytes):
-        self.address = address
-        self.register = register
-        self.bytes = bytes
+        self._read_arguments[register] = [address, register, bytes]
+        return self._read_result[register]
+
+    def get_readfrom_mem_arguments(self, register):
+        return self._read_arguments[register]
 
     def writeto_mem(self, address, register, register_bytes):
-        self.address = address
-        self.register = register
-        self.register_bytes = register_bytes
+        self._write_arguments[register] = [address, register_bytes]
+
+    def get_writeto_mem_arguments(self, register):
+        return self._write_arguments[register]
--- tests/test_read.py
+++ tests/test_read.py
@@ -6,10 +6,36 @@ from ina219 import INA219
 class TestRead(unittest.TestCase):
 
     def setUp(self):
-        self.ina = INA219(0.1, I2C(2))
+        self.i2c = I2C(2)
+        self.ina = INA219(0.1, self.i2c)
 
     def test_read_32v(self):
+        self._set_expected_bus_voltage_read(bytes([0xfa, 0x00]))
+
         self.assertEqual(self.ina.voltage(), 32)
+        self._assert_read_bus_voltage_args()
+
+    def test_read_16v(self):
+        self._set_expected_bus_voltage_read(bytes([0x7d, 0x00]))
+
+        self.assertEqual(self.ina.voltage(), 16)
+        self._assert_read_bus_voltage_args()
+
+    def test_read_4_808v(self):
+        self._set_expected_bus_voltage_read(bytes([0x25, 0x92]))
+
+        self.assertEqual(self.ina.voltage(), 4.808)
+        self._assert_read_bus_voltage_args()
+
+    def _set_expected_bus_voltage_read(self, bytes):
+        self.i2c.set_readfrom_mem_result(self.ina.__REG_BUSVOLTAGE, bytes)
+
+    def _assert_read_bus_voltage_args(self):
+        self.assertEqual(self._get_read_args(self.ina.__REG_BUSVOLTAGE),
+                         [self.ina.__ADDRESS, self.ina.__REG_BUSVOLTAGE, 2])
+
+    def _get_read_args(self, register):
+        return self.i2c.get_readfrom_mem_arguments(register)
 
 
 if __name__ == '__main__':
